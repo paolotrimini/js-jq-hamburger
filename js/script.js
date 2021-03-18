4f39ab70030f95a674f66eee714fcd43ad3ca7e6
paolotrimini <paolo.trimini@gmail.com>
2021-03-18 23:09:32 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 // Scrivere solo la parte con jQuery senza modiﬁcare HTML e CSS.
 
 
+// VERSIONE 1
+
 // Visualizzo il menu
 
 $('.fas').click(function() { // azione (click)
@@ -15,4 +17,21 @@ $('.fas').click(function() { // azione (click)
 $('.close').click(function() {  // azione (click)
 
     $('.hamburger-menu').removeClass('active'); // "rimuovo" classe 'active'
+});
+
+
+// VERSIONE 2
+
+// Visualizzo il menu
+
+$('.fa-times').click(function() { // hide x
+
+    $('.hamburger-menu').hide();
+});
+
+// Nascondo il menu
+
+$('.fa-bars').click(function() {   // show lines
+
+    $('.hamburger-menu').show();
 });
